2607814552a8661dc79a018c6d1aeb5423dead26
GenuHo <2500820792@qq.com>
2026-09-16 22:10:53 +0800

    refactor(components): [composite-search] forward only passed props to the inner filter groups

--- packages/components/composite-search/src/composite-search-filter-multiple.tsx
+++ packages/components/composite-search/src/composite-search-filter-multiple.tsx
@@ -1,19 +1,30 @@
 import '@tailor-more/t-more-theme-chalk/composite-search-filter-multiple.less'
 
 import { useNamespace } from '@tailor-more/t-more-hooks'
-import { defineComponent } from 'vue'
+import { defineComponent, getCurrentInstance } from 'vue'
 import type { TmCompositeSearchFilterMultipleProps } from './composite-search-filter-multiple-type'
+import tCheckboxGroupProps from 'tdesign-vue-next/es/checkbox/checkbox-group-props'
 
 export default defineComponent<TmCompositeSearchFilterMultipleProps>({
   name: 'TmCompositeSearchFilterMultiple',
-  inheritAttrs: false,
-  setup(props, { attrs, slots }) {
+  props: {
+    ...tCheckboxGroupProps,
+  },
+  setup(_, { attrs, slots }) {
     const ns = useNamespace('composite-search-filter-multiple')
-
+    const instance = getCurrentInstance()
     return () => {
+      // 直接拿setup的props拿到的会含有上层没有传的属性的key也在其中，会导致下层有问题，下层是根据是否有对应的key判定用户是否有传对应的字段
+      // 具体见tdesign-vue-next的useVModel
+      // https://github.com/Tencent/tdesign-vue-next/blob/develop/packages/shared/hooks/useVModel/index.ts
+      const props = instance?.vnode.props
       return (
         <div class={ns.b()}>
-          <TCheckboxGroup v-slots={slots} {...attrs}></TCheckboxGroup>
+          <TCheckboxGroup
+            v-slots={slots}
+            {...props}
+            {...attrs}
+          ></TCheckboxGroup>
         </div>
       )
     }
--- packages/components/composite-search/src/composite-search-filter-single.tsx
+++ packages/components/composite-search/src/composite-search-filter-single.tsx
@@ -1,28 +1,29 @@
 import '@tailor-more/t-more-theme-chalk/composite-search-filter-single.less'
 
 import { useNamespace } from '@tailor-more/t-more-hooks'
-import { defineComponent } from 'vue'
+import { defineComponent, getCurrentInstance } from 'vue'
 import type { TmCompositeSearchFilterSingleProps } from './composite-search-filter-single-type'
+import tRadioGroupProps from 'tdesign-vue-next/es/radio/radio-group-props'
 
-/**
- * 单选筛选
- * 这里 defineComponent 直接声明出来Props，然后往下传参直接用 attrs 传给TRadioGroup，这样保证
- * 如果直接传props给下层，props中会有所有的属性的key，会产生的问题是，肯定会传相关的value和modelValue到下层
- * 下层的 useVModel 判定 getCurrentInstance().vnode.props 中是否有 modelValue model-value value，就肯定会包含其中，按照优先级就会使用 modelValue
- * 但是这个modelValue所在层是封装层（从上到下依次是使用层、封装层、被封装层），update:modelValue 通过{...attrs}会到使用层，使用层没有相关定义就没有效果切换选中的效果了
- */
 export default defineComponent<TmCompositeSearchFilterSingleProps>({
   name: 'TmCompositeSearchFilterSingle',
-  inheritAttrs: false,
-  setup(props, { attrs, slots }) {
+  props: {
+    ...tRadioGroupProps,
+  },
+  setup(_, { attrs, slots }) {
     const ns = useNamespace('composite-search-filter-single')
-
+    const instance = getCurrentInstance()
     return () => {
+      // 直接拿setup的props拿到的会含有上层没有传的属性的key也在其中，会导致下层有问题，下层是根据是否有对应的key判定用户是否有传对应的字段
+      // 具体见tdesign-vue-next的useVModel
+      // https://github.com/Tencent/tdesign-vue-next/blob/develop/packages/shared/hooks/useVModel/index.ts
+      const props = instance?.vnode.props
       return (
         <div class={ns.b()}>
           {
             <TRadioGroup
               v-slots={slots}
+              {...props}
               {...attrs}
               direction="vertical"
             ></TRadioGroup>
